cd44dcbb5e13ac70c363c002bda876a66de65a03
simon.wagner <simon_wagner1@gmx.net>
2024-04-14 21:14:19 +0200

    blatt 3: aufgabe 4 algorithmus

--- Blatt_3/Aufgabe_4/algo.py
+++ Blatt_3/Aufgabe_4/algo.py
@@ -1,5 +1,3 @@
-
-
 def recursiveImpl(n, m):
     if n == 0:
         return m + 1
@@ -9,14 +7,48 @@ def recursiveImpl(n, m):
         return recursiveImpl(n-1, recursiveImpl(n, m-1))
 
 
-def iterativeImpl(n, m):
-    resultStackN = []
-    resultStackM = []
-    resultStack = []
-    while action := opstack.pop():
-        match action:
-            case "normal_op":
-                if n == 0:
-                    result = m + 1
-                elif m == 0 and n >= 1:
-                    opstack.push("normal_op")
+def iterativeImpl(m, n):
+    stack = [m]
+    while stack:
+        m = stack.pop()
+        if m == 0:
+            n = n + 1
+        elif n == 0:
+            n = 1
+            stack.append(m-1)
+        else:
+            n = n - 1
+            stack.extend([m-1, m])
+
+    return n
+
+
+def main():
+    for n in range(2, 10):
+        for m in range(2, 10):
+
+            #rec = recursiveImpl(n, m)
+            it = iterativeImpl(n, m)
+            #print(f"{n=},{m=}: {rec:<3} {it:<3}")
+            print(f"{n=},{m=}: {it:<3}")
+# max output:
+"""
+n=2,m=2: 7  
+n=2,m=3: 9  
+n=2,m=4: 11 
+n=2,m=5: 13 
+n=2,m=6: 15 
+n=2,m=7: 17 
+n=2,m=8: 19 
+n=2,m=9: 21 
+n=3,m=2: 29 
+n=3,m=3: 61 
+n=3,m=4: 125
+n=3,m=5: 253
+n=3,m=6: 509
+n=3,m=7: 1021
+n=3,m=8: 2045
+n=3,m=9: 4093
+"""
+
+main()
