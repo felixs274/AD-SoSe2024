d43723718098f0ca922eaf9b41627b54fab481a6
felixs274 <felix.scholzen27@gmail.com>
2024-04-28 22:52:33 +0200

    4.1

--- Blatt_5/Aufgabe_4/main.py
+++ Blatt_5/Aufgabe_4/main.py
@@ -1,4 +1,10 @@
-import time
+#
+# In Zusammenarbeit zwischen Daniel Heisig, Felix Scholzen & Simon Wagner
+#
+
+
+
+import time, random
 
 
 
@@ -48,6 +54,79 @@ def variante2(M, N):
 
 
 
+def matrix_multiply(a,b):
+
+    c = []
+    for i in range(0,len(a)):
+        temp=[]
+        for j in range(0,len(b[0])):
+            s = 0
+            for k in range(0,len(a[0])):
+                s += a[i][k]*b[k][j]
+            temp.append(s)
+        c.append(temp)
+
+    return c
+
+
+
+# STRASSEN ALGORITHM -------------------------------------------------------------------
+
+def matrix_add(A, B):
+    return [[A[i][j] + B[i][j] for j in range(len(A[0]))] for i in range(len(A))]
+
+def matrix_subtract(A, B):
+    return [[A[i][j] - B[i][j] for j in range(len(A[0]))] for i in range(len(A))]
+
+def strassen_matrix_multiplication(A, B):
+    if len(A) == 1:
+        return [[A[0][0] * B[0][0]]]
+    else:
+        # Dividing the matrices into quadrants
+        mid = len(A) // 2
+
+        A11 = [row[:mid] for row in A[:mid]]
+        A12 = [row[mid:] for row in A[:mid]]
+        A21 = [row[:mid] for row in A[mid:]]
+        A22 = [row[mid:] for row in A[mid:]]
+
+        B11 = [row[:mid] for row in B[:mid]]
+        B12 = [row[mid:] for row in B[:mid]]
+        B21 = [row[:mid] for row in B[mid:]]
+        B22 = [row[mid:] for row in B[mid:]]
+
+        # Calculating p1 to p7:
+        p1 = strassen_matrix_multiplication(matrix_add(A11, A22), matrix_add(B11, B22))
+        p2 = strassen_matrix_multiplication(matrix_add(A21, A22), B11)
+        p3 = strassen_matrix_multiplication(A11, matrix_subtract(B12, B22))
+        p4 = strassen_matrix_multiplication(A22, matrix_subtract(B21, B11))
+        p5 = strassen_matrix_multiplication(matrix_add(A11, A12), B22)
+        p6 = strassen_matrix_multiplication(matrix_subtract(A21, A11), matrix_add(B11, B12))
+        p7 = strassen_matrix_multiplication(matrix_subtract(A12, A22), matrix_add(B21, B22))
+
+        # Re-combining the four parts of the result matrix
+        C11 = matrix_add(matrix_subtract(matrix_add(p1, p4), p5), p7)
+        C12 = matrix_add(p3, p5)
+        C21 = matrix_add(p2, p4)
+        C22 = matrix_add(matrix_subtract(matrix_add(p1, p3), p2), p6)
+
+        # Constructing the whole matrix from halves
+        top = [C11[i] + C12[i] for i in range(len(C11))]
+        bottom = [C21[i] + C22[i] for i in range(len(C21))]
+        return top + bottom
+
+# ------------------------------------------------------------------------------------------------------
+
+
+def timer(fun, a, b):
+    start_time = time.time()
+    res = fun(a, b)
+    end_time = time.time()
+    duration = end_time - start_time
+    print(duration)   
+    return res
+
+
 def main():
 
     # Matrix M & N
@@ -56,9 +135,16 @@ def main():
 
     N = [[5, 6], [7, 8]]
 
-    print(variante1(M, N))
-    print(variante2(M, N))
-
+    #print(variante1(M, N))
+    #print(variante2(M, N))
+    
   
+    # For Strassen ---------------------------------------------------------------------------------------
+
+    A = [[random.randint(1, 100) for _ in range(100)] for _ in range(100)]
+    B = [[random.randint(1, 100) for _ in range(100)] for _ in range(100)]
+
+    #print(timer(strassen_matrix_multiplication, A, B))
+    timer(matrix_multiply, A, B)
 
 main()
